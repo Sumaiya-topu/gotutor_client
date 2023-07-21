fcf71ca74ebf43e6b3f4752b092c29a1c6a712d7
Sumaiya-topu <topusumaiya@gmail.com>
2023-07-22 02:21:20 +0600

    working on navbar

--- src/Router/PublicRoutes.js
+++ src/Router/PublicRoutes.js
@@ -1,7 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../Authentication/Login/Login";
+import Home from "../Pages/Home/Home";
 
 const publicRoutes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
   {
     path: "/login",
     element: <Login></Login>,
