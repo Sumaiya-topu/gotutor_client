7fdd9df548338a86d11c8f392169993e191dbd48
Sumaiya-topu <topusumaiya@gmail.com>
2023-07-21 19:41:52 +0600

    necessary package installed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is react app</h1>
+    <div>
+      <h1 className=" text-rose-500">This is react app</h1>
     </div>
   );
 }
