cc23a4b0a9b723fbecdb39745fbcc2652b734b26
Sumaiya-topu <topusumaiya@gmail.com>
2023-07-24 21:11:59 +0600

    added a simple nav bar

--- src/Pages/Home/Navbar/Navbar.js
+++ src/Pages/Home/Navbar/Navbar.js
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return <div>this is nav</div>;
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <div>
+      <>
+        <nav className="main-nav">
+          {/* 1st logo part  */}
+          <div className="logo">
+            <h2>
+              <span>G</span>o<span>T</span>utor
+            </h2>
+          </div>
+
+          {/* 2nd menu part  */}
+          <div
+            className={
+              showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+            }
+          >
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">about</NavLink>
+              </li>
+              <li>
+                <NavLink to="/service">services</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">contact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            </ul>
+          </div>
+
+          {/* 3rd social media links */}
+          <div className="social-media">
+            <ul className="social-media-desktop">
+              <li>
+                <a
+                  href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                  target="_thapa"
+                >
+                  <FaFacebookSquare className="facebook" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/thapatechnical/"
+                  target="_thapa"
+                >
+                  <FaInstagramSquare className="instagram" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                  target="_thapa"
+                >
+                  <FaYoutubeSquare className="youtube" />
+                </a>
+              </li>
+            </ul>
+
+            {/* hamburget menu start  */}
+            <div className="hamburger-menu">
+              <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                <GiHamburgerMenu />
+              </a>
+            </div>
+          </div>
+        </nav>
+
+        {/* hero section  */}
+        {/* <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Thapa Technical</h1>
+      </section> */}
+      </>
+    </div>
+  );
 };
 
 export default Navbar;
