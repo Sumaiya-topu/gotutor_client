5d0d5cf91d0f17dad5cfc06668443a823369d31c
Sumaiya-topu <topusumaiya@gmail.com>
2023-08-25 17:56:29 +0600

    working on search bar

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,23 +1,33 @@
 import { Button, Input } from "@material-tailwind/react";
 import React from "react";
-import "./Hero.css";
+// import "./Hero.css";
+import { useForm } from "react-hook-form";
 
 const Hero = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleSearch = (data) => {
+    console.log("Search form", data);
+  };
   return (
-    <div className=" heroBg h-screen flex items-center">
+    <div className=" h-screen flex items-center">
       <div className=" w-1/2 mx-auto bg-black/20 rounded-lg ">
         {" "}
-        <form action="" className=" p-10">
+        <form onSubmit={handleSubmit(handleSearch)} className=" p-10">
           <div className="flex gap-2">
             <Input
-              //   {...register("institution", { required: true })}
+              {...register("class")}
               color="indigo"
               label="Class"
               className=""
               type="text"
             ></Input>
             <Input
-              //   {...register("institution", { required: true })}
+              {...register("medium")}
               color="indigo"
               label="Medium"
               className=""
@@ -26,20 +36,21 @@ const Hero = () => {
           </div>
           <div className="flex gap-2 mt-2">
             <Input
-              //   {...register("institution", { required: true })}
+              {...register("localArea")}
               color="indigo"
               label="Local Area"
               className=""
               type="text"
             ></Input>
             <Input
-              //   {...register("institution", { required: true })}
+              {...register("district")}
               color="indigo"
               label="District"
-              className=""
+              className=" "
               type="text"
             ></Input>
             <Button
+              type="submit"
               className="shadow-none bg-[#7839ff] hover:shadow-none hover:-translate-y-1"
               fullWidth
             >
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ComplexNavbar from "../../Components/Navbar/ComplexNavbar";
 import Hero from "../../Components/Hero/Hero";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className=" heroBg">
       <ComplexNavbar></ComplexNavbar>
       <Hero></Hero>
     </div>
