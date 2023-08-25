e12ab866c779c628c4b94396373920c49afb5abe
Sumaiya-topu <topusumaiya@gmail.com>
2023-08-26 03:33:30 +0600

    showcase section design

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -14,61 +14,130 @@ const Hero = () => {
     console.log("Search form", data);
   };
   return (
-    <div className=" h-screen flex items-center justify-evenly">
-      {/* <p>vaaa</p>
+    <div>
+      <div className=" h-[600px] flex gap-10  w-2/3 mx-auto">
+        {/* <p>vaaa</p>
       <p>vaaa</p> */}
-      <div className=" bg-black/20 rounded-lg ">
-        {" "}
-        <form onSubmit={handleSubmit(handleSearch)} className=" p-10">
-          <div className="flex gap-2">
-            <Input
-              {...register("class")}
-              color="indigo"
-              label="Class"
-              className=""
-              type="text"
-            ></Input>
-            <Input
-              {...register("medium")}
-              color="indigo"
-              label="Medium"
-              className=""
-              type="text"
-            ></Input>
+        <div className="flex items-center  w-full">
+          <div className=" bg-black/20 rounded-lg w-full">
+            {" "}
+            <form onSubmit={handleSubmit(handleSearch)} className=" p-10">
+              <div className="flex gap-2">
+                <Input
+                  {...register("class")}
+                  color="indigo"
+                  label="Class"
+                  className=""
+                  type="text"
+                ></Input>
+                <Input
+                  {...register("medium")}
+                  color="indigo"
+                  label="Medium"
+                  className=""
+                  type="text"
+                ></Input>
+              </div>
+              <div className="flex gap-2 mt-2">
+                <Input
+                  {...register("localArea")}
+                  color="indigo"
+                  label="Local Area"
+                  className=""
+                  type="text"
+                ></Input>
+                <Input
+                  {...register("district")}
+                  color="indigo"
+                  label="District"
+                  className=" "
+                  type="text"
+                ></Input>
+                <Button
+                  type="submit"
+                  className="shadow-none bg-[#7839ff] hover:shadow-none hover:-translate-y-1"
+                  fullWidth
+                >
+                  Search
+                </Button>
+              </div>
+            </form>
           </div>
-          <div className="flex gap-2 mt-2">
-            <Input
-              {...register("localArea")}
-              color="indigo"
-              label="Local Area"
-              className=""
-              type="text"
-            ></Input>
-            <Input
-              {...register("district")}
-              color="indigo"
-              label="District"
-              className=" "
-              type="text"
-            ></Input>
-            <Button
-              type="submit"
-              className="shadow-none bg-[#7839ff] hover:shadow-none hover:-translate-y-1"
-              fullWidth
-            >
-              Search
-            </Button>
-          </div>
-        </form>
+        </div>
+        <div className="mt-36 ">
+          <h3 className="text-white/90 text-end font-sans">
+            <span className="text-3xl">Welcome to</span> - <br />{" "}
+            <span className="text-6xl ">
+              Go<span className="text-[#7839ff] font-bold">Tutor</span>
+            </span>{" "}
+          </h3>
+          <p className="text-white/90 mt-5 text-end">
+            <span className="text-xl font-sans">Your Path to Academic</span>{" "}
+            <br />{" "}
+            <span className="text-7xl text-[#ff6739] font-semibold font-serif">
+              {" "}
+              Excellence!
+            </span>
+          </p>
+        </div>
       </div>
-      <div className="">
-        <h3 className="text-white/90 text-end">
-          <span className="text-2xl">Welcome to</span> - <br />{" "}
-          <span className="text-6xl">
-            Go<span className="text-[#7839ff] font-bold">Tutor</span>
-          </span>{" "}
-          <br /> Your Path to Academic Excellence!
-        </h3>
+      <div className="w-3/4 mx-auto">
+        <div className="flex justify-center ">
+          <div className="w-full flex items-end ">
+            <div>
+              <h1 className=" text-5xl font-serif text-white/90 mr-7 mb-5 text-right">
+                Why Choose Us?
+              </h1>
+              <div className="bg-white/90 rounded-l-lg ">
+                <p className="px-10 pt-10 pb-[100px] text-right font-sans text-black/80   ">
+                  We are providing the best teachers.{" "}
+                  <span className="text-[#7839ff] text-xl">
+                    Around 200 users have <br /> chosen us.
+                  </span>{" "}
+                  We are nurturing our teachers by providing batches to reach
+                  them to their desired audiance.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-10 w-full bg-[#7839ff] rounded-t-lg ">
+            <div className=" text-white/90 pl-10 pt-10 font-sans  ">
+              <h3 className="text-xl mb-3">Expert Educators:</h3>
+              <p className=" text-justify">
+                Our team of experienced and dedicated educators are experts in
+                their respective fields. They are passionate about teaching and
+                are committed to helping you grasp even the most complex
+                concepts.
+              </p>
+            </div>
+            <div className=" text-white/90 pr-10 pt-10 font-sans ">
+              <h3 className="text-xl mb-3">Personalized Learning:</h3>
+              <p className=" text-justify">
+                We understand that every student has unique learning needs.
+                That's why we tailor our teaching approach to suit your
+                individual pace, learning style, and goals. Your success is our
+                priority.
+              </p>
+            </div>{" "}
+            <div className=" text-white/90 pl-10 pb-10 font-sans">
+              <h3 className="text-xl mb-3">Comprehensive Curriculum:</h3>
+              <p className=" text-justify">
+                Our team of experienced and dedicated educators are experts in
+                their respective fields. They are passionate about teaching and
+                are committed to helping you grasp even the most complex
+                concepts.
+              </p>
+            </div>{" "}
+            <div className=" text-white/90 pb-10 pr-10 font-sans">
+              <h3 className="text-xl mb-3">Supportive Environment:</h3>
+              <p className=" text-justify">
+                Beyond academics, we foster a supportive and motivating
+                environment. We believe in nurturing not only intellectual
+                growth but also personal development.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
